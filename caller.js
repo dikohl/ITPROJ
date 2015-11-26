91cb1897340bde7389e6f17026e36df279a13a2c
Dimitri Kohler <gorotix@hotmail.com>
2015-11-26 11:46:39 +0100

    getting games to client works now

--- caller.js
+++ caller.js
@@ -14,7 +14,27 @@ exports.getFriends = function(user) {
 exports.getGames = function(user, callback){
 	var path = '/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey + '&steamid=' + user.slice(1) + '&include_appinfo=1&format=json';
 	var hostname = 'api.steampowered.com';
-	
+	getGamesFromSteam(path, hostname, function(response){
+		
+		console.log(response);
+		if(response.type == 'games'){
+			callback({
+				type: response.type,
+				games: response.games
+			});
+		}
+		else{
+			callback({
+				type: response.type,
+				state: response.state
+			});
+		}	
+	});
+}
+
+var getGamesFromSteam = function (path, hostname, callback){
+	//500 if unknown ID
+	//200 if wrong APIKEY
 	var options = {
 		hostname: hostname,
 		path: path,
@@ -23,93 +43,45 @@ exports.getGames = function(user, callback){
 		        }
 	}
 	
-	callback = function(response){
-		var games_list = [];
-		var output = '';
+	var games_list = [];
+	
+	http.get(options).on('response', function(response){
 		
-		response.on('data', function(chunk){
-			output += chunk;
-		});
-
-		response.on('end',function(){
-
-			var data = JSON.parse(output);
-			var games = data.response.games;
-			for (var i = 0; i < games.length; i++){
-				var game = {
-					appid: games[i].appid,
-					name: games[i].name
-				}
-				games_list.push(game);
-			}
+		console.log('-----------------------------------------');
+		console.log('STATUS: ' + response.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(response.headers));
+		console.log('-----------------------------------------');
+		
+		if(response.statusCode != 200){
 			callback({
-				type: 'games',
-				games: games_list
+				type: 'error',
+				state: response.statusCode
 			});
-		});
-	}
-	http.request(options, callback).end();
-	
-	/*if (suc){
-		callback({
-			type: 'succ',
-			state: 'Request successful'
-		});
-	}
-	else{
-		callback({
-			type: 'err',
-			state: 'An error uccurred'
-		});
-	}*/
+		}
+		else{
+			var output = '';
+			response.on('data', function(chunk){
+				output += chunk;
+			});
 
-}
-
-function getGamesFromSteam(path){
-	/*console.log('-----------------------------------------');
-	console.log('STATUS: ' + response.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(response.headers));
-	console.log('-----------------------------------------');*/
-	
-	//500 if unknown ID
-	//200 if wrong APIKEY
-	
+			response.on('end',function(){
+				var data = JSON.parse(output);
+				var games = data.response.games;
+				for (var i = 0; i < games.length; i++){
+					var game = {
+						appid: games[i].appid,
+						name: games[i].name
+					}
+					games_list.push(game);
+				}
+				callback({
+					type: 'games',
+					games: games_list
+				});
+			});
+		}
+	});
 }
 
 function getFriendsFromSteam(path){
-	/*console.log('-----------------------------------------');
-	console.log('STATUS: ' + response.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(response.headers));
-	console.log('-----------------------------------------');*/
-	
-	//500 if unknown ID
-	//200 if wrong APIKEY
-	var options = {
-		hostname: 'api.steampowered.com',
-		path: path,
-		headers: {
-		            accept: 'application/json'
-		        }
-	}
-	
-	callback = function(response){
-		
-		if(response.statusCode != 200){
-			return;
-		}
-			
-		var output = '';
-		response.on('data', function(chunk){
-			output += chunk;
-		});
-		
-		response.on('end',function(){
-			var friends_list = [];
-			var data = JSON.parse(output);
-			//console.log(data.response.games[0]);
-			
-		});
-	}
-	http.request(options, callback).end();
-	
 }
