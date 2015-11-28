3fca4268eb08a828f12ee6fe61aac7f8c27e7fad
Dimitri Kohler <gorotix@hotmail.com>
2015-11-28 12:40:48 +0100

    requesting friends works again

--- caller.js
+++ caller.js
@@ -1,28 +1,59 @@
 var http = require('http');
 var apiKey = process.argv.slice(2);
+var hostname = 'api.steampowered.com';
 
-exports.getData = function(user, game, callback){
+exports.getFriends = function(user, appId, callback) {
+	var path = '/ISteamUser/GetFriendList/v0001/?key=' + apiKey + '&steamid=' + user + '&relationship=friend&format=json';
+	getFriendsFromSteam(path, hostname, function(response){
+		if(response.type == 'friends'){
+			//get games of every freind
+			for(var i = 0; i < response.friends.length; i++){
+				console.log(response.friends[i]);
+				/*getGamesFiltered(response.friends[i].steamid, appId, function(callback){
+					
+					if(response.type == 'games'){
+						callback({
+							type: response.type,
+							games: response.games
+						});
+					}
+					else{
+						callback({
+							type: response.type,
+							state: response.state
+						});
+					}	
+				});*/
+			}
+		}
+		//handle error
+		else{
+			callback({
+				type: response.type,
+				state: response.state
+			});
+		}	
+		
+	});
 	
 }
 
-exports.getFriends = function(user) {
-	var path = '/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey + '&steamid=' + user + '&include_appinfo=1&format=json';
-	var friends = getFriendsFromSteam(path);
-	
+function getGamesFiltered(user, appId, callback){
+	//'/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey + '&steamid=' + user + '&include_appinfo=1&format=json&appids_filter[0]=' + appId;
 }
+
 
 exports.getGames = function(user, callback){
 	var path = '/IPlayerService/GetOwnedGames/v0001/?key=' + apiKey + '&steamid=' + user + '&include_appinfo=1&format=json';
-	var hostname = 'api.steampowered.com';
 	getGamesFromSteam(path, hostname, function(response){
-		
-		console.log(response);
+		//handle callback
 		if(response.type == 'games'){
 			callback({
 				type: response.type,
 				games: response.games
 			});
 		}
+		//handle error
 		else{
 			callback({
 				type: response.type,
@@ -35,6 +66,8 @@ exports.getGames = function(user, callback){
 var getGamesFromSteam = function (path, hostname, callback){
 	//500 if unknown ID
 	//200 if wrong APIKEY
+	
+	//options for http.get
 	var options = {
 		hostname: hostname,
 		path: path,
@@ -46,20 +79,20 @@ var getGamesFromSteam = function (path, hostname, callback){
 	var games_list = [];
 	
 	http.get(options).on('response', function(response){
+		//callback function
 		
-		console.log('-----------------------------------------');
-		console.log('STATUS: ' + response.statusCode);
-		console.log('HEADERS: ' + JSON.stringify(response.headers));
-		console.log('-----------------------------------------');
-		
+		//if something else than ok (200) is returned
 		if(response.statusCode != 200){
 			callback({
 				type: 'error',
 				state: response.statusCode
 			});
 		}
+		
+		//handle data
 		else{
 			var output = '';
+			//gather JSON chunks
 			response.on('data', function(chunk){
 				output += chunk;
 			});
@@ -67,6 +100,7 @@ var getGamesFromSteam = function (path, hostname, callback){
 			response.on('end',function(){
 				var data = JSON.parse(output);
 				var games = data.response.games;
+				//get clean list of games with ID and name
 				for (var i = 0; i < games.length; i++){
 					var game = {
 						appid: games[i].appid,
@@ -74,6 +108,7 @@ var getGamesFromSteam = function (path, hostname, callback){
 					}
 					games_list.push(game);
 				}
+				//give back clean list of games
 				callback({
 					type: 'games',
 					games: games_list
@@ -83,5 +118,49 @@ var getGamesFromSteam = function (path, hostname, callback){
 	});
 }
 
-function getFriendsFromSteam(path){
+function getFriendsFromSteam(path, hostname, callback){
+	
+	//options for http.get
+	var options = {
+		hostname: hostname,
+		path: path,
+		headers: {
+		            accept: 'application/json'
+		        }
+	}
+
+	http.get(options).on('response', function(response){
+		/*
+		console.log(JSON.stringify(response.headers))
+		console.log(response.statusCode);
+		*/
+		
+		
+		//callback function
+	
+		//if something else than ok (200) is returned
+		if(response.statusCode != 200){
+			callback({
+				type: 'error',
+				state: response.statusCode
+			});
+		}
+		//handle data
+		else{
+			var output = '';
+			//gather JSON chunks
+			response.on('data', function(chunk){
+				output += chunk;
+			});
+
+			response.on('end',function(){
+				var data = JSON.parse(output);
+				//give back list of friends
+				callback({
+					type: 'friends',
+					friends: data.friendslist.friends
+				});
+			});
+		}
+	});
 }
